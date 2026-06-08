3cef3ae1290a89831bd7efca1f051d379db25a02
judye <lijudy82@gmail.com>
2026-06-08 17:25:59 +1000

    Updated the "delivery method" and "payment method" so users can only select one option at a time. Achieved this through using Javascript.

--- js/checkout.js
+++ js/checkout.js
@@ -1,3 +1,19 @@
-function toggleButton(button) {
-    button.classList.toggle("active")
-}
+function toggleDelivery(clickedButton) {
+    const buttons = document.querySelectorAll(".delivery-btn");
+
+    buttons.forEach(button => {
+    button.classList.remove("active")
+    })
+
+clickedButton.classList.add("active");
+} 
+
+function toggleButton(clickedButton) {
+    const buttons = document.querySelectorAll(".selection-btn");
+
+    buttons.forEach(button => {
+    button.classList.remove("active")
+    })
+
+clickedButton.classList.add("active");
+} 
